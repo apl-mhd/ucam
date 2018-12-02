1eff0f98c8c409e4630be2141f1c2de5cd8ca0d2
orko.mhd <orko.mhd@gmail.com>
2018-12-02 10:59:57 +0600

    add popup

---
+++ content.js
@@ -0,0 +1,12 @@
+console.log("apel mahmud");
+
+var para = document.getElementsByTagName('h4');
+
+chrome.runtime.onMessage.addListener(gotMessage);
+
+function gotMessage(message, sender, sendResponse) {
+	console.log(message.txt);
+	if (message.txt === "hello"){
+		document.write('a');
+	}
+}
---
+++ popup.js
@@ -0,0 +1,31 @@
+function setup () 
+{
+	let userInput = select('#userInput');
+
+	userInput.text(newText);
+
+	function newText(){
+
+		let params = {
+
+			active : true,
+			currentWindow : true
+		}
+
+		chrome.tabs.query(params,gotTab);
+
+		function gotTab(tabs){
+
+			console.log(tabs);
+
+		let message = userInput.value();
+		let msg = {
+			txt : "hello"
+		} 
+
+		chrome.tabs.sendMessage(tabs[0].id,msg);
+	}
+
+
+	}
+}
