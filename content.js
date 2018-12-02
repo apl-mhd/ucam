da1a051b78f8d5e47f771040ca934e477cd3a1e5
orko.mhd <orko.mhd@gmail.com>
2018-12-02 23:29:14 +0600

    Add three otption Good, Average, BellowAverage

--- content.js
+++ content.js
@@ -4,42 +4,14 @@ console.log("Apel Mahmud");
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
 
-  	if (request.greeting == 'best') {
-  		console.log('best teacher ever');
-  	}
-  	//console.log(request.greeting);
-    console.log(sender.tab ?
-                "from a content script:" + sender.tab.url :
-                "from the extension");
-    if (request.greeting == "hello")
-      sendResponse({farewell: "goodbye"});
+  	// if (request.greeting == 'best') {
+  	// 	console.log('best teacher ever');
+  	// }
+  	console.log(request.greeting);
+    
+    // if (request.greeting == "hello")
+    //   sendResponse({farewell: "goodbye"});
+
+
   });
 
-
-
-/*
-chrome.runtime.onMessage.addListener(gotMessage);
-function gotMessage (request, sender, sendResponse) {
-	
-
-	console.log(request);
-}
-
-/*
-
-
-/*
-console.log("apel mahmud");
-
-var para = document.getElementsByTagName('h4');
-
-chrome.runtime.onMessage.addListener(gotMessage);
-
-function gotMessage(message, sender, sendResponse) {
-	console.log(message.txt);
-	if (message.txt === "hello"){
-		document.write('a');
-	}
-}
-
-*/
--- popup.js
+++ popup.js
@@ -9,59 +9,43 @@ function senData () {
 
 
 document.addEventListener('DOMContentLoaded', function () {
-  document.querySelector('button').addEventListener('click', senData);
+  //document.querySelector('button').addEventListener('click', senData);
+
+  /*Good*/
+
+  document.querySelector('#good').addEventListener('click',function(){
+  	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+  	chrome.tabs.sendMessage(tabs[0].id, {greeting: "good"}, function(response) {
+    console.log("send success");
+  		});
+	});
+  	
+  });
+
+  /*Average*/
+  document.querySelector('#average').addEventListener('click',function(){
+  	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+  	chrome.tabs.sendMessage(tabs[0].id, {greeting: "average"}, function(response) {
+    console.log("send success");
+  		});
+	});
+  	
+  });
+
+  /*Bellow Average*/
+  document.querySelector('#baverage').addEventListener('click',function(){
+  	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+  	chrome.tabs.sendMessage(tabs[0].id, {greeting: "baverage"}, function(response) {
+    console.log("send success");
+  		});
+	});
+  	
+  });
+
+
   
-});
-
-console.warn('lol');
+}); /* DOMContentLoaded */
 
 
-
-// chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-//   chrome.tabs.sendMessage(tabs[0].id, {greeting: "why you click me"}, function(response) {
-//     console.log("send success");
-//   });
-// });
-
-/*
-chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-    sendResponse({
-        data: "I am fine, thank you. How is life in the background?"
-    }); 
-});
-*/
-/*
-let userinput = select('#userinput');
-userinput.text(changeText);
-
-console.log("got tetxt");
-
-function changeText(){
-
-	console.log("got change Text");
-
-	chrome.tabs.query(params,gotTab);
-
-	let params = {
-			active : true,
-			currentWindow : true
-		}
-
-		function gotTab(tabs){
-			console.log("got tab");
-			
-			let message = userInput.value();
-
-			console.log(message);
-
-			chrome.tabs.sendMessage(tabs[0].id, message);
-
-		}
-
-}
-
-*/
-
-//chrome.tabs.sendMessage("message");
 
 
