71ca793ac30ae2739ab254def35798d63d890407
orko.mhd <orko.mhd@gmail.com>
2018-12-04 13:55:25 +0600

    add radio button on testwebsite folder

--- content.js
+++ content.js
@@ -4,6 +4,39 @@ console.log("Apel Mahmud");
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
 
+  	if (request.greeting == 'good') {
+
+  			fillRadio(0);	
+  	}
+
+  	if (request.greeting == 'average') {
+
+  			fillRadio(1);
+  		
+  	}
+
+  	if (request.greeting == 'baverage') {
+
+  		fillRadio(2);
+  	}
+
+
+
+  	function fillRadio(n){
+
+  		var x =  document.querySelectorAll('input[type="radio"][name="car"]');
+		x[n].checked=true;
+
+		var x =  document.querySelectorAll('input[type="radio"][name="motor"]');
+		x[n].checked=true;
+
+		var x =  document.querySelectorAll('input[type="radio"][name="mobile"]');
+		x[n].checked=true;
+  	}
+
+
+
+
   	// if (request.greeting == 'best') {
   	// 	console.log('best teacher ever');
   	// }
