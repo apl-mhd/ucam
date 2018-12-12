102811c46980afd840360f755868f267dcc701ff
orko.mhd <orko.mhd@gmail.com>
2018-12-12 23:35:36 +0600

    Test for UCAM

--- content.js
+++ content.js
@@ -1,6 +1,6 @@
 
 
-console.log("Orin is <3");
+console.log("Orin <3");
 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
@@ -12,27 +12,54 @@ chrome.runtime.onMessage.addListener(
 
   	if (request.greeting == 'average') {
 
-  			fillRadio(1);
+  			fillRadio(2);
   		
   	}
 
   	if (request.greeting == 'baverage') {
 
-  		fillRadio(2);
+  		fillRadio(4);
   	}
 
 
 
   	function fillRadio(n){
 
-  		var x =  document.querySelectorAll('input[type="radio"][name="car"]');
+  		var x =  document.querySelectorAll('input[type="radio"][name="ctl00$MainContainer$rbtTheory1"]');
 		x[n].checked=true;
+		var x =  document.querySelectorAll('input[type="radio"][name="ctl00$MainContainer$rbtTheory2"]');
+		x[n].checked=true;
+		var x =  document.querySelectorAll('input[type="radio"][name="ctl00$MainContainer$rbtTheory3"]');
+		x[n].checked=true;
+    var x =  document.querySelectorAll('input[type="radio"][name="ctl00$MainContainer$rbtTheory4"]');
+    x[n].checked=true;
+    var x =  document.querySelectorAll('input[type="radio"][name="ctl00$MainContainer$rbtTheory5"]');
+    x[n].checked=true;
+    var x =  document.querySelectorAll('input[type="radio"][name="ctl00$MainContainer$rbtTheory6"]');
+    x[n].checked=true;
+    var x =  document.querySelectorAll('input[type="radio"][name="ctl00$MainContainer$rbtTheory7"]');
+    x[n].checked=true;
 
-		var x =  document.querySelectorAll('input[type="radio"][name="motor"]');
-		x[n].checked=true;
+    var x =  document.querySelectorAll('input[type="radio"][name="ctl00$MainContainer$rbtTheory8"]');
+    x[n].checked=true;
+    var x =  document.querySelectorAll('input[type="radio"][name="ctl00$MainContainer$rbtTheory9"]');
+    x[n].checked=true;
+    var x =  document.querySelectorAll('input[type="radio"][name="ctl00$MainContainer$rbtTheory10"]');
+    x[n].checked=true;
+    var x =  document.querySelectorAll('input[type="radio"][name="ctl00$MainContainer$rbtTheory11"]');
+    x[n].checked=true;
+    var x =  document.querySelectorAll('input[type="radio"][name="ctl00$MainContainer$rbtTheory12"]');
+    x[n].checked=true;
+    var x =  document.querySelectorAll('input[type="radio"][name="ctl00$MainContainer$rbtTheory13"]');
+    x[n].checked=true;
+    var x =  document.querySelectorAll('input[type="radio"][name="ctl00$MainContainer$rbtTheory14"]');
+    x[n].checked=true;
+    var x =  document.querySelectorAll('input[type="radio"][name="ctl00$MainContainer$rbtTheory15"]');
+    x[n].checked=true;
+    var x =  document.querySelectorAll('input[type="radio"][name="ctl00$MainContainer$rbtTheory16"]');
+    x[n].checked=true;
 
-		var x =  document.querySelectorAll('input[type="radio"][name="mobile"]');
-		x[n].checked=true;
+
   	}
 
 
